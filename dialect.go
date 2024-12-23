74cefab630202593995459229be3c77328229ab3
agabek309 <=>
2024-12-23 09:50:28 +0500

    start sqlite dialect

--- dialect.go
+++ dialect.go
@@ -3,6 +3,7 @@ package dblayer
 // Реализации для разных БД
 type MySQLDialect struct{}
 type PostgresDialect struct{}
+type SQLiteDialect struct{}
 
 type Dialect interface {
 	GetAutoIncrement() string
