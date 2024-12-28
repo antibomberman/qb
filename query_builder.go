c817f8958533922edaa0501567419319b3bbb656
agabek309 <=>
2024-12-28 21:23:05 +0500

    fix errors

--- query_builder.go
+++ query_builder.go
@@ -3,6 +3,7 @@ package dblayer
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"reflect"
@@ -47,6 +48,75 @@ type Executor interface {
 	NamedExec(query string, arg interface{}) (sql.Result, error)
 	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
 	SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error
+}
+
+// execGet выполняет запрос и получает одну запись
+func (qb *QueryBuilder) execGet(dest interface{}, query string, args ...interface{}) (bool, error) {
+	query = qb.rebindQuery(query)
+	err := qb.getExecutor().Get(dest, query, args...)
+	if errors.Is(err, sql.ErrNoRows) {
+		return false, nil
+	}
+	return err == nil, err
+}
+
+// execSelect выполняет запрос и получает множество записей
+func (qb *QueryBuilder) execSelect(dest interface{}, query string, args ...interface{}) (bool, error) {
+	query = qb.rebindQuery(query)
+	err := qb.getExecutor().Select(dest, query, args...)
+	if errors.Is(err, sql.ErrNoRows) {
+		return false, nil
+	}
+	return err == nil, err
+}
+
+// execExec выполняет запрос без возврата данных
+func (qb *QueryBuilder) execExec(query string, args ...interface{}) error {
+	query = qb.rebindQuery(query)
+	_, err := qb.getExecutor().Exec(query, args...)
+	return err
+}
+
+// execGetContext выполняет запрос с контекстом и получает одну запись
+func (qb *QueryBuilder) execGetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) (bool, error) {
+	query = qb.rebindQuery(query)
+	if ex, ok := qb.getExecutor().(interface {
+		GetContext(context.Context, interface{}, string, ...interface{}) error
+	}); ok {
+		err := ex.GetContext(ctx, dest, query, args...)
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+		return err == nil, err
+	}
+	return false, fmt.Errorf("executor doesn't support context")
+}
+
+// execSelectContext выполняет запрос с контекстом и получает множество записей
+func (qb *QueryBuilder) execSelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) (bool, error) {
+	query = qb.rebindQuery(query)
+	if ex, ok := qb.getExecutor().(interface {
+		SelectContext(context.Context, interface{}, string, ...interface{}) error
+	}); ok {
+		err := ex.SelectContext(ctx, dest, query, args...)
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+		return err == nil, err
+	}
+	return false, fmt.Errorf("executor doesn't support context")
+}
+
+// execExecContext выполняет запрос с контекстом
+func (qb *QueryBuilder) execExecContext(ctx context.Context, query string, args ...interface{}) error {
+	query = qb.rebindQuery(query)
+	if ex, ok := qb.getExecutor().(interface {
+		ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+	}); ok {
+		_, err := ex.ExecContext(ctx, query, args...)
+		return err
+	}
+	return fmt.Errorf("executor doesn't support context")
 }
 
 // On регистрирует обработчик события
@@ -137,7 +207,7 @@ func (qb *QueryBuilder) getDriverName() string {
 	// return ""
 }
 
-func (qb *QueryBuilder) buildQuery(head string) (string, []interface{}) {
+func (qb *QueryBuilder) buildBodyQuery(head string) (string, []interface{}) {
 	var args []interface{}
 	var sql strings.Builder
 
@@ -193,7 +263,7 @@ func (qb *QueryBuilder) buildSelectQuery() (string, []interface{}) {
 	}
 
 	head := fmt.Sprintf("SELECT %s FROM %s", selectClause, tableName)
-	body, args := qb.buildQuery(head)
+	body, args := qb.buildBodyQuery(head)
 	return head + body, args
 }
 
@@ -229,7 +299,7 @@ func (qb *QueryBuilder) buildUpdateQuery(data interface{}, fields []string) (str
 
 	head := fmt.Sprintf("UPDATE %s SET %s", qb.table, strings.Join(sets, ", "))
 
-	body, bodyArgs := qb.buildQuery(head)
+	body, bodyArgs := qb.buildBodyQuery(head)
 	args = append(args, bodyArgs...)
 	return head + body, args
 
@@ -250,7 +320,7 @@ func (qb *QueryBuilder) buildUpdateMapQuery(data map[string]interface{}) (string
 	}
 	head := fmt.Sprintf("UPDATE %s SET %s", qb.table, strings.Join(sets, ", "))
 
-	body, bodyArgs := qb.buildQuery(head)
+	body, bodyArgs := qb.buildBodyQuery(head)
 	args = append(args, bodyArgs...)
 	return head + body, args
 
