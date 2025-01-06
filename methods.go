34b049e6618e59483f673699a39b7f095776b42b
agabek309 <=>
2025-01-06 09:20:27 +0500

    update

--- methods.go
+++ methods.go
@@ -8,29 +8,29 @@ import (
 
 // Query Builder
 func (d *DBLayer) Query(table string) *q.Builder {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.Query(table)
 }
 
 func (d *DBLayer) Begin() (*q.Transaction, error) {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.Begin()
 }
 func (d *DBLayer) BeginContext(ctx context.Context) (*q.Transaction, error) {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.BeginContext(ctx)
 }
 
 func (d *DBLayer) Transaction(fn func(*q.Transaction) error) error {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.Transaction(fn)
 }
 func (d *DBLayer) TransactionContext(ctx context.Context, fn func(*q.Transaction) error) error {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.TransactionContext(ctx, fn)
 }
 func (d *DBLayer) Raw(query string, args ...interface{}) *q.RawQuery {
-	qb := q.NewQueryBuilder(d.DB, d.DriverName)
+	qb := q.New(d.DB, d.DriverName)
 	return qb.Raw(query, args...)
 }
 
